5b09400c362f42571943e1bd7ac8391e303a6fc8
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2025-04-11 21:48:49 +0200

    bug fixes

--- src/components/GlassHeader.tsx
+++ src/components/GlassHeader.tsx
@@ -84,15 +84,15 @@ export default function GlassHeader() {
               {MENU_ITEMS.map(
                 (item, index) => (
                   <motion.a
-                    key={item}
-                    href={`#${item}`}
+                    key={item.id}
+                    href={`#${item.id}`}
                     className="transition-colors hover:text-foreground/80 text-foreground/60 py-2"
                     onClick={toggleMenu}
                     initial={{ opacity: 0, x: -20 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.2, delay: index * 0.1 }}
                   >
-                    {item.charAt(0).toUpperCase() + item.slice(1)}
+                    {item.text.charAt(0).toUpperCase() + item.text.slice(1)}
                   </motion.a>
                 )
               )}
