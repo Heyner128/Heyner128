f2d81e7e03c152c110e4e2508893198545f1b59c
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2025-07-10 23:24:28 +0200

    compose config

--- bin/aws-deploy.sh
+++ bin/aws-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PORT=4321
-docker compose up --detach --build
+docker compose up --detach --build --remove-orphans
 sleep 20
 curl --fail --retry 5 --retry-delay 10 --retry-connrefused http://localhost:${PORT}
