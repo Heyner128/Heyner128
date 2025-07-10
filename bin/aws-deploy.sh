591ff862fa48372fb2ba5c0d8d026ebb6762dabe
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2025-07-11 00:08:51 +0200

    compose config

--- bin/aws-deploy.sh
+++ bin/aws-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export PORT=9099
 PROJECT_NAME=portfolio
-docker compose -p ${PROJECT_NAME} down
 docker compose -p ${PROJECT_NAME} up --detach --build
 sleep 20
 curl --fail --retry 5 --retry-delay 10 --retry-connrefused http://localhost:${PORT}
