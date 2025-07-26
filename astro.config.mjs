17c32f87a6f4f0392839b539079e53d3f849cfdd
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2025-07-26 19:55:20 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,10 +16,6 @@ export default defineConfig({
 
   site: "https://heyner.me",
 
-  redirects: {
-    "/files/[...name]": "https://portfolio-public-resources-1.s3.eu-west-3.amazonaws.com/[...name]",
-  },
-
   output: 'server',
 
   i18n: {
---
+++ resources.config.js
@@ -0,0 +1,1 @@
+export const S3_BUCKET_URL = "https://portfolio-public-resources-1.s3.eu-west-3.amazonaws.com"
